7065f99686a20a76edc564e570b2d3827ac7770d
Raymond Bourges <raymond.bourges@asso-cocktail.fr>
2015-10-26 11:04:15 +0100

    refactor: websocket on page

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -8,23 +8,6 @@
             'ppController',
             'homeController'
         ])
-        .factory('Ws', function($websocket, $location) {
-            // Open a WebSocket connection
-            var ws = $websocket('ws://' + $location.host() + ':' + /*$location.port()*/8080 + '/team?teamName=gfc');
-
-            var message = {data: {}};
-
-            ws.onMessage(function(mess) {
-                message.data = JSON.parse(mess.data);
-            });
-
-            return {
-                message: message,
-                status: function() {
-                    return ws.readyState;
-                }
-            };
-        })
         .config(
             function($routeProvider) {
                 $routeProvider.
--- src/main/resources/static/app/wip/wipController.js
+++ src/main/resources/static/app/wip/wipController.js
@@ -2,16 +2,23 @@
     'use strict';
 
     angular
-        .module('wipController', [])
+        .module('wipController', [
+            'ngWebSocket'
+        ])
         .controller('WipCtrl', WipCtrl);
 
     WipCtrl.$inject = [
         '$scope',
-        'Ws'
+        '$websocket',
+        '$location'
     ];
-    function WipCtrl($scope, Ws) {
+    function WipCtrl($scope, $websocket, $location) {
         var vm = this;
         vm.test = "ICI";
-        vm.ws = Ws;
+        vm.message = {data: {}};
+        var ws = $websocket('ws://' + $location.host() + ':' + /*$location.port()*/8080 + '/team?teamName=gfc');
+        ws.onMessage(function(mess) {
+            vm.message.data = JSON.parse(mess.data);
+        });
     }
 })();
